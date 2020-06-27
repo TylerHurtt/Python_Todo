7d97cb7ea7c55968afeca7153f5390f9b48f169e
Tyler Hurtt <TylerAdamHurtt@gmail.com>
2020-06-27 16:47:59 -0700

    Added flask_migrate for database version control and added a completed column to the existing todos table in the database

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 import sys
 
 app = Flask(__name__)
 
-app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Tyler:Pesthlos!2772@localhost:5432/python_todo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.debug = True
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, nullable=False, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo{self.id} {self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/')
