4c886ca339b3b2585e285250dcf5193bb5c6c582
Tyler Hurtt <TylerAdamHurtt@gmail.com>
2020-06-28 13:58:25 -0700

    Created update logic in index.js for checkboxes to send fetch request on change to the server

--- static/index.js
+++ static/index.js
@@ -2,10 +2,12 @@
 const form = document.getElementById('form');
 form.onsubmit = function (e) {
   e.preventDefault();
+  const todoDescription = document.getElementById('description').value;
+  // if (todoDescription != '') {
   fetch('/todos/create', {
     method: 'POST',
     body: JSON.stringify({
-      description: document.getElementById('description').value,
+      description: todoDescription.toLowerCase(),
     }),
     headers: {
       'Content-Type': 'application/json',
@@ -14,7 +16,7 @@ form.onsubmit = function (e) {
     .then((res) => res.json())
     .then((data) => {
       const listElement = document.createElement('li');
-      listElement.innerHTML = data.description;
+      listElement.innerText = data.description;
       document.getElementById('todo-list').appendChild(listElement);
       document.getElementById('error').className = 'hidden';
       document.getElementById('description').value = '';
@@ -24,4 +26,32 @@ form.onsubmit = function (e) {
       error.className = 'error';
       error.innerHTML = JSON.stringify(err.message);
     });
+  // }
 };
+
+// Checked-Box
+const checkboxes = document.querySelectorAll('.checkbox');
+const numberOfCheckboxes = checkboxes.length;
+let i = 0;
+for (i; i < numberOfCheckboxes; i++) {
+  const checkbox = checkboxes[i];
+  checkbox.onchange = function (e) {
+    const isCompleted = e.target.checked;
+    const id = e.target.dataset.id;
+    fetch(`/todos/${id}/set-completed`, {
+      method: 'POST',
+      body: JSON.stringify({
+        completed: isCompleted,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => (document.getElementById('error').className = 'hidden'))
+      .catch(() => {
+        const error = document.getElementById('error');
+        error.className = 'error';
+        error.innerHTML = JSON.stringify(err.message);
+      });
+  };
+}
